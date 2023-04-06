5ae3eb2ec6bf5a69149af42199f5bdfcd95822b8
nicooh <n.ciano@studenti.uniba.it>
2023-04-06 12:23:27 +0200

    Fix Area

--- pages/area/[slug].tsx
+++ pages/area/[slug].tsx
@@ -33,53 +33,56 @@ export default function AreaPage({area, services}: AreaPageProps) {
             <section
                 className={"relative before:block before:absolute before:w-full before:h-full before:top-0 before:bg-black before:opacity-40 bg-cover bg-center"}
                 style={{backgroundImage: `url("${area.img}")`}} id={"abo"}>
-                <motion.div className="container flex flex-wrap justify-between lg:py-24 py-12 backdrop-blur-md">
-                    <motion.div variants={fadeInUp} className="w-full order-1">
-                        <BreadCrumbs mappedPaths={config} showHome={true} transformDynamicPath={path => {
-                            if (path === "[slug]") {
-                                return area.slug
-                            }
-                            return path;
-                        }}/>
-                        <div>
-                            <div className="relative w-fit mx-auto">
+                <motion.div className="lg:py-24 py-12 backdrop-blur-md">
+                    <motion.div className={"container flex flex-wrap justify-between"}>
+                        <motion.div variants={fadeInUp} className="w-full order-1">
+                            <BreadCrumbs mappedPaths={config} showHome={true} transformDynamicPath={path => {
+                                if (path === "[slug]") {
+                                    return area.slug
+                                }
+                                return path;
+                            }}/>
+                            <div>
+                                <div className="relative w-fit mx-auto">
+                                    <motion.div variants={blockReveal} whileInView="final" viewport={{once: true}}
+                                                className="blockOverText bg-gray-100"></motion.div>
+                                    <motion.h1 variants={blockTextReveal} initial="initial" whileInView="final"
+                                               className={"text-center text-white"}
+                                               viewport={{once: true}}> {area.name}</motion.h1>
+                                </div>
+                                <div className={"w-fit max-w-[600px] relative mt-2 mx-auto"}>
+                                    <motion.div variants={blockReveal} whileInView="final" viewport={{once: true}}
+                                                className="blockOverText bg-gray-100"></motion.div>
+                                    <motion.div dangerouslySetInnerHTML={{__html: area.description ? area.description : ""}}
+                                                variants={blockTextReveal} initial="initial" whileInView="final"
+                                                viewport={{once: true}} className={"text-white text-center"}/>
+                                </div>
+                            </div>
+
+                            <div className={"w-fit relative mt-2"}>
                                 <motion.div variants={blockReveal} whileInView="final" viewport={{once: true}}
                                             className="blockOverText bg-gray-100"></motion.div>
-                                <motion.h1 variants={blockTextReveal} initial="initial" whileInView="final"
-                                           className={"text-center text-white"}
-                                           viewport={{once: true}}> {area.name}</motion.h1>
-                            </div>
-                            <div className={"w-fit max-w-[600px] relative mt-2 mx-auto"}>
-                                <motion.div variants={blockReveal} whileInView="final" viewport={{once: true}}
-                                            className="blockOverText bg-gray-100"></motion.div>
-                                <motion.div dangerouslySetInnerHTML={{__html: area.description ? area.description : ""}}
-                                            variants={blockTextReveal} initial="initial" whileInView="final"
-                                            viewport={{once: true}} className={"text-white text-center"}/>
-                            </div>
-                        </div>
+                                <ul className={styles.activities}>{area.activities?.map((activity: any, index: number) => {
+                                    return <li key={index} className={styles.activity}>
+                                        <div className={styles.content}>
+                                            <h5>{activity.title}</h5>
+                                            <motion.div dangerouslySetInnerHTML={{__html: activity.description}}
+                                                        variants={blockTextReveal} initial="initial" whileInView="final"
+                                                        viewport={{once: true}} className={""}/>
+                                        </div>
 
-                        <div className={"w-fit relative mt-2"}>
-                            <motion.div variants={blockReveal} whileInView="final" viewport={{once: true}}
-                                        className="blockOverText bg-gray-100"></motion.div>
-                            <ul className={styles.activities}>{area.activities?.map((activity: any, index: number) => {
-                                return <li key={index} className={styles.activity}>
-                                    <div className={styles.content}>
-                                        <h5>{activity.title}</h5>
-                                        <motion.div dangerouslySetInnerHTML={{__html: activity.description}}
-                                                    variants={blockTextReveal} initial="initial" whileInView="final"
-                                                    viewport={{once: true}} className={""}/>
-                                    </div>
+                                    </li>
+                                })}</ul>
+                            </div>
+                        </motion.div>
 
-                                </li>
-                            })}</ul>
-                        </div>
+                        <motion.div variants={fadeInUp}
+                                    className={"w-full relative lg:w-5/12 bg-none lg:bg-cover bg-center flex flex-row items-start order-last lg:order-3"}
+                                    style={{backgroundImage: `url("${area.img}")`}}>
+                            <img className={"max-h-[200px] block lg:hidden w-full object-cover"} src={area.img}/>
+                        </motion.div>
                     </motion.div>
 
-                    <motion.div variants={fadeInUp}
-                                className={"w-full relative lg:w-5/12 bg-none lg:bg-cover bg-center flex flex-row items-start order-last lg:order-3"}
-                                style={{backgroundImage: `url("${area.img}")`}}>
-                        <img className={"max-h-[200px] block lg:hidden w-full object-cover"} src={area.img}/>
-                    </motion.div>
                 </motion.div>
             </section>
 
